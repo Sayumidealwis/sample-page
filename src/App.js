cd874d66ad7d8cb273417d447c554073add4d744
Kavindu Thilan <kavinduthilan2000@outlook.com>
2023-12-08 10:34:40 +0530

    App.js is modified

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import Header from "./Components/Header"
+import UserProfile from "./Components/UserProfile"
 
 function App() {
   return (
     <div>
       <Header/>
-      
+      <UserProfile/>
     </div>
   )
 }
