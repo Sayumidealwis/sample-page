b0a76b32258626bf7b01ffe0dff12fea5a4972dd
Kavindu Thilan <kavinduthilan2000@outlook.com>
2023-12-08 11:17:34 +0530

    edited by kavindu

--- src/Components/UserProfile.jsx
+++ src/Components/UserProfile.jsx
@@ -3,7 +3,6 @@ function UserProfile() {
       <div>
         <h1>This is User Profile</h1>
         <p>All of the user details will be displayed here.....</p>
-        <p>Welcome</p>
       </div>
     )
   }
