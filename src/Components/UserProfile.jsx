77996e2f3fa15497ae68df4d294e8b4078af932b
Kavindu Thilan <kavinduthilan2000@outlook.com>
2023-12-08 10:19:13 +0530

    User profile first edited by kavindu

---
+++ src/Components/UserProfile.jsx
@@ -0,0 +1,10 @@
+function UserProfile() {
+    return (
+      <div>
+        <h1>This is User Profile</h1>
+        <p>All of the user details will be displayed here.....</p>
+      </div>
+    )
+  }
+
+  export default UserProfile;
