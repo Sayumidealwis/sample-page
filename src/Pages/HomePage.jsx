89bf83bd01ab3bfadb4097e776684c28aa2e6c73
Sanjana Amarasinghe <120704911+Nalajitha99@users.noreply.github.com>
2023-12-08 15:51:45 +0530

    modified Home page by sanjana

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import "./HomePage.css";
 
 const HomePage = () => {
   return (
-    <div>HomePage</div>
+    <div>
+      <div className='home_logo'>
+        <img src='https://cdn.enfsolar.com/ID/logo/5e2674a1a7550.jpg?v=1' alt='cardImage'/>
+      </div>
+    </div>
   )
 }
 
