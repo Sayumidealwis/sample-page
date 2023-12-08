9862e2bb61b25702889a230ea94cdb14d68fa3f2
Sanjana Amarasinghe <120704911+Nalajitha99@users.noreply.github.com>
2023-12-08 12:10:07 +0530

    Header files modified by sanjana

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,13 +1,31 @@
 import "./Header.css";
+import React, { useState, useEffect } from 'react';
 
-import React from 'react'
+const Header = () => {
+  const [currentTime, setCurrentTime] = useState(new Date());
 
-function Header() {
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentTime(new Date());
+    }, 1000);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+
   return (
+    <header>
     <div className="header_container">
-        <h1>RIVIDCO PVT.LTD</h1>
+        <div className="image">
+            <img src="https://cdn.enfsolar.com/ID/logo/5e2674a1a7550.jpg?v=1" alt="cardImage"/>
+        </div>
+        <div className="date">{currentTime.toLocaleTimeString()}<br/>
+        {currentTime.toDateString()}
+        </div>
     </div>
-  )
-}
+    </header>
+  );
+};
 
-export default Header
+export default Header;
