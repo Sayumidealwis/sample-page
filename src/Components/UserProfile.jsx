9426a27b99030332943938710109686865c3a64c
Kavindu Thilan <kavinduthilan2000@outlook.com>
2023-12-10 00:23:16 +0530

    Add new paragraph

--- src/Components/UserProfile.jsx
+++ src/Components/UserProfile.jsx
@@ -3,7 +3,7 @@ function UserProfile() {
       <div>
         <h1>This is User Profile</h1>
         <p>All of the user details will be displayed here.....</p>
-        <p>Welcome all</p>
+        <p>Welcome</p>
       </div>
     )
   }
