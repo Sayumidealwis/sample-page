05b6539773d0d260a820a24661b235666ac172ac
Kavindu Thilan <kavinduthilan2000@outlook.com>
2023-12-10 09:13:01 +0530

    Add form to EditTaskPage

--- src/Pages/EditTaskPage.jsx
+++ src/Pages/EditTaskPage.jsx
@@ -4,6 +4,37 @@ const EditTaskPage = () => {
   return (
     <div>
       <h1>This is edit a task page</h1>
+      <form>
+        Description :<input type="text"/><br/><br/>
+        Category  :
+        <select>
+          <option>option 1</option>
+          <option>option 2</option>
+          <option>option 3</option>
+        </select>
+        <br/><br/>
+        Project Regarding :
+        <select>
+          <option>option 1</option>
+          <option>option 2</option>
+        </select>
+        <br/><br/>
+        Requested By :
+        <select>
+          <option>option 1</option>
+          <option>option 2</option>
+        </select><br/><br/>
+        Assigned To :
+        <select>
+          <option>option 1</option>
+          <option>option 2</option>
+        </select><br/><br/>
+        Status :<select>
+          <option>option 1</option>
+          <option>option 2</option>
+        </select><br/><br/>
+        Comment : <textarea cols={10} rows={10}></textarea>
+      </form>
     </div>
   )
 }
