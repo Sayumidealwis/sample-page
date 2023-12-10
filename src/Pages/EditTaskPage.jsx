f7044d4aa09209d101f3dfc657281f04f6be6d5c
Kavindu Thilan <kavinduthilan2000@outlook.com>
2023-12-10 20:18:50 +0530

    added EditTaskPage.css

--- src/Pages/EditTaskPage.jsx
+++ src/Pages/EditTaskPage.jsx
@@ -1,39 +1,55 @@
 import React from 'react'
+import './EditTaskPage.css'
 
 const EditTaskPage = () => {
+  const handlesubmit =()=>{
+    alert("task detail edited");
+  }
   return (
-    <div>
+    <div className="edit-task-page">
       <h1>This is edit a task page</h1>
       <form>
-        Description :<input type="text"/><br/><br/>
-        Category  :
-        <select>
+        <label className="form-label">Description:</label>
+        <input type="text" disabled/><br/><br/>
+
+        <label className="form-label">Category:</label>
+        <select disabled>
           <option>option 1</option>
           <option>option 2</option>
-          <option>option 3</option>
+          <option selected>option 3</option>
         </select>
         <br/><br/>
-        Project Regarding :
-        <select>
+
+        <label className="form-label">Project Regarding:</label>
+        <select disabled>
           <option>option 1</option>
-          <option>option 2</option>
+          <option selected>option 2</option>
         </select>
         <br/><br/>
-        Requested By :
+
+        <label className="form-label">Requested By:</label>
+        <select disabled>
+          <option selected>option 1</option>
+          <option>option 2</option>
+        </select><br/><br/>
+
+        <label className="form-label">Assigned To:</label>
         <select>
           <option>option 1</option>
           <option>option 2</option>
         </select><br/><br/>
-        Assigned To :
-        <select>
+        
+        <label className="form-label">Status:</label>
+        <select required>
           <option>option 1</option>
           <option>option 2</option>
         </select><br/><br/>
-        Status :<select>
-          <option>option 1</option>
-          <option>option 2</option>
-        </select><br/><br/>
-        Comment : <textarea cols={10} rows={10}></textarea>
+        
+        <label className="form-label">Comment:</label>
+        <textarea cols={30} rows={10}></textarea>
+        <br/><br/>
+
+        <button type='submit'onClick={handlesubmit()}>Save</button>
       </form>
     </div>
   )
