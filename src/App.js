a3f5a1c08305987ab4645eb966b0e71205183f1b
S.M.Jayawardhana <mjayawardhana19240@gmail.com>
2023-12-13 12:59:17 +0530

    new styles 2023-12-13

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
 import React from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import HomePage from './Pages/HomePage'
-import TaskList from './Pages/TaskList'
+import TaskPage from './Pages/TaskPage'
 import Sidebar from './Components/Sidebar'
 import EditTaskPage from './Pages/EditTaskPage'
 import AddTaskPage from './Pages/AddTaskPage'
 import ViewTaskPage from './Pages/ViewTaskPage'
 import HistoryTaskPage from './Pages/HistoryTask'
-import Header from './Components/Header'
 
 function App() {
   return (
     <BrowserRouter>
-      <div>
-        {/* <Header /> */}
+      <div style={{ display: 'flex' }}>
         <Sidebar />
         <div>
           <Routes>
             <Route path="/" element={<HomePage />} />
-            <Route path="/tasklist" element={<TaskList />} />
+            <Route path="/tasklist" element={<TaskPage />} />
             <Route path="/taskview" element={<ViewTaskPage />} />
             <Route path="/taskadd" element={<AddTaskPage />} />
             <Route path="/taskedit" element={<EditTaskPage />} />
@@ -27,11 +25,6 @@ function App() {
         </div>
       </div>
     </BrowserRouter>
-
-
-
-
-
   )
 }
 
